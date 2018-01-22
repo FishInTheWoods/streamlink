b9bf747943a33e4dbe448ae13c935c80f82f018f
unai <unai.gg77@gmail.com>
2018-01-22 16:34:54 +0100

    changed some setting, this is tricky

--- src/streamlink/plugins/youtube.py
+++ src/streamlink/plugins/youtube.py
@@ -211,8 +211,8 @@ class YouTube(Plugin):
 	        # Issues when trying to download Youtube videos, looks like 'el' value is key here
 	        # https://github.com/Tyrrrz/YoutubeExplode/issues/66#issuecomment-348685419
             #"el": "player_embedded"
-	        #"el": "detailpage" 
-	        "el": "embedded"
+	        "el": "detailpage" 
+	        #"el": "embedded"
         }
         res = http.get(API_VIDEO_INFO, params=params, headers=HLS_HEADERS)
         return parse_query(res.text, name="config", schema=_config_schema)
